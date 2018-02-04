ff0f5b3962e3f78a4a3caa2ace5524aaff604982
David Barnes <barnesdavidj@gmail.com>
2018-02-03 22:39:44 -0500

    Update views with more boiler plate. Break repeated sections into partials. Update routes with correct syntax for route params. Remove Maste Layout since Express does not support Layouts by default. Add a site js file.

--- app/routes/note.server.routes.js
+++ app/routes/note.server.routes.js
@@ -3,7 +3,7 @@ module.exports = function (app) {
     app.get('/', note.index);
     app.get('/create', note.create);
     app.post('/store', note.store);
-    app.get('/edit/{id}', note.edit);
-    app.post('/update/{id}', note.update);
-    app.post('/destroy/{id}', note.destroy);
+    app.get('/edit/:id', note.edit);
+    app.post('/update/:id', note.update);
+    app.post('/destroy/:id', note.destroy);
 };
